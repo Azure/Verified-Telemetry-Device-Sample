1d6c73ddbb5c3bd4e0a8dad410ab837b14cf33d6
Ryan Winter <ryanwinter@outlook.com>
2021-01-25 21:41:16 -0800

    convert libs to submodules

--- core/lib/netxduo/addons/azure_iot/azure-sdk-for-c/sdk/tests/iot/pnp/test_az_iot_pnp_client.h
+++
@@ -1,8 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// SPDX-License-Identifier: MIT
-
-int test_az_iot_pnp_client();
-int test_az_iot_pnp_client_commands();
-int test_az_iot_pnp_client_sas();
-int test_az_iot_pnp_client_telemetry();
-int test_az_iot_pnp_client_property();
