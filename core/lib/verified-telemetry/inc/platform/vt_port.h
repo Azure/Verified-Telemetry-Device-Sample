11402a7362108245188e8674c4f4155270eccba1
Nishant-Shrivastava <77138655+Nishant-Shrivastava@users.noreply.github.com>
2021-02-01 17:03:50 +0530

    Applied Clang

--- core/lib/verified-telemetry/inc/platform/vt_port.h
+++ core/lib/verified-telemetry/inc/platform/vt_port.h
@@ -4,30 +4,27 @@
 #ifndef _VT_PORT_H
 #define _VT_PORT_H
 
-
-#ifdef STM32F4XX
+#ifdef MXCHIP_AZ3166
 #include "stm32f4xx_hal.h"
 #endif
-#ifdef STM32L4XX
+#ifdef STM_BL475EIOT01A
 #include "stm32l4xx_hal.h"
 #endif
 
-#ifdef STM32F4XX
-#define GPIO_PORT_TYPEDEF		GPIO_TypeDef
-#define GPION_PIN_TYPEDEF		uint16_t
-#define ADC_CONTROLLER_TYPEDEF	ADC_HandleTypeDef
-#define ADC_CHANNEL_TYPEDEF		uint32_t
-#define TIMER_HANDLE_TYPEDEF    TIM_HandleTypeDef
+#ifdef MXCHIP_AZ3166
+#define GPIO_PORT_TYPEDEF      GPIO_TypeDef
+#define GPION_PIN_TYPEDEF      uint16_t
+#define ADC_CONTROLLER_TYPEDEF ADC_HandleTypeDef
+#define ADC_CHANNEL_TYPEDEF    uint16_t
+#define TIMER_HANDLE_TYPEDEF   TIM_HandleTypeDef
 #endif
 
-#ifdef STM32L4XX
-#define GPIO_PORT_TYPEDEF		GPIO_TypeDef
-#define GPION_PIN_TYPEDEF		uint16_t
-#define ADC_CONTROLLER_TYPEDEF	ADC_HandleTypeDef
-#define ADC_CHANNEL_TYPEDEF		uint32_t
-#define TIMER_HANDLE_TYPEDEF    TIM_HandleTypeDef
+#ifdef STM_BL475EIOT01A
+#define GPIO_PORT_TYPEDEF      GPIO_TypeDef
+#define GPION_PIN_TYPEDEF      uint16_t
+#define ADC_CONTROLLER_TYPEDEF ADC_HandleTypeDef
+#define ADC_CHANNEL_TYPEDEF    long
+#define TIMER_HANDLE_TYPEDEF   TIM_HandleTypeDef
 #endif
-
-
 
 #endif
