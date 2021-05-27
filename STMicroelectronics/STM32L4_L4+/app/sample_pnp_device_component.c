18bb666f2187b1829e4f3013f44b41c8f98de64f
Ryan Winter <ryanwinter@outlook.com>
2021-05-26 22:00:24 -0700

    increase delay between applying VCC to moisture sensor and taking a reading.

--- STMicroelectronics/STM32L4_L4+/app/sample_pnp_device_component.c
+++ STMicroelectronics/STM32L4_L4+/app/sample_pnp_device_component.c
@@ -130,7 +130,7 @@ UINT get_sensor_data(SAMPLE_PNP_DEVICE_COMPONENT* handle)
 
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_9, GPIO_PIN_SET);
     HAL_GPIO_WritePin(GPIOB, GPIO_PIN_8, GPIO_PIN_SET);
-    HAL_Delay(10);
+    tx_thread_sleep(20);
 
     UINT soilMoisture1ADCData = adc_read(&hadc1, ADC_CHANNEL_1);
     UINT soilMoisture2ADCData = adc_read(&hadc1, ADC_CHANNEL_2);
